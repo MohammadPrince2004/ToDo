6cf9a8a792a8e28a43c53e818538b251aa832a57
MohammadPrince2004 <mostafaprince2008@gmail.com>
2025-08-27 11:59:10 +0300

    Add files via upload

--- Model/SelectedBorderModel.dart
+++ Model/SelectedBorderModel.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../ViewModel/MainDataProvider.dart';
+
+class SelectedBorderModel extends ChangeNotifier {
+  int index = 0;
+
+  void select(int new_val) {
+
+    index=new_val;
+    notifyListeners();
+
+  }
+}
